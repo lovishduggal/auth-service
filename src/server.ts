f047ef857f92e21eba57605c8ba0dca52f3ed79d
Lovish Duggal <lovishduggal11@gmail.com>
2024-03-19 09:45:34 +0530

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-function login(username: string, password: string) {
+function login(username: string, password: string): string {
     if (username === 'admin' && password === 'admin') {
         return 'Login Success';
     } else {
         return 'Login Failed';
     }
 }
-
-console.log(login('admin', 'admin'));
-console.log(login('admin', 'admin1'));
+login('admin', 'admin');
