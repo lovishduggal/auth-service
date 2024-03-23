1b370e9c0af8d0ac681e0ead4d40db1af59a6ff8
Lovish Duggal <lovishduggal11@gmail.com>
2024-03-23 09:04:58 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
     const PORT = Config.PORT;
 
     try {
         app.listen(PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`Server is running on port ${PORT}`);
+            logger.info(`Server is running on port ${PORT}`);
         });
-    } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        // Terminate the Node.js process with an exit code of 1.
-        process.exit(1);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            setTimeout(() => process.exit(1), 1000);
+        }
     }
 };
 
