f73e62c50ff10ccdb1edc0e5e7e3fa3def93d44d
Lovish Duggal <lovishduggal11@gmail.com>
2024-03-30 09:00:44 +0530

    write first test

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import { HttpError } from 'http-errors';
 import logger from './config/logger';
+import authRouter from './routes/auth';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.use('/auth', authRouter);
 
 app.use(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
