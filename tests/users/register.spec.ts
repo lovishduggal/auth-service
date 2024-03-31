298268d249d005307e81853d24037f16611a2360
Lovish Duggal <lovishduggal11@gmail.com>
2024-03-31 10:04:40 +0530

    add test for persist user

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,6 +1,25 @@
 import request from 'supertest';
 import app from '../../src/app';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../src/config/data-source';
+import { truncateTables } from '../utils';
+import { User } from '../../src/entity/User';
 describe('POST /auth/register', () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        //* Clear the database before each test
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe('Given all fields', () => {
         it('should return the 201 status code', async () => {
             //* We can follow a formula for writing any test that is AAA
@@ -50,6 +69,12 @@ describe('POST /auth/register', () => {
             await request(app).post('/auth/register').send(userData);
 
             //* Assert:
+            const userRepository = connection.getRepository(User);
+            const user = await userRepository.find();
+            expect(user).toHaveLength(1);
+            expect(user[0].firstName).toBe(userData.firstName);
+            expect(user[0].lastName).toBe(userData.lastName);
+            expect(user[0].email).toBe(userData.email);
         });
     });
     describe('Fields are missing', () => {});
