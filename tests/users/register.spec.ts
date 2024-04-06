e22cc16aa0e5e9725ec20203f2b23bbf725dc7b5
Lovish Duggal <lovishduggal11@gmail.com>
2024-04-06 07:59:34 +0530

    add test for password hashing and unique email

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -111,9 +111,46 @@ describe('POST /auth/register', () => {
             await request(app).post('/auth/register').send(userData);
             //* Assert:
             const userRepository = connection.getRepository(User);
-            const user = await userRepository.find();
-            expect(user[0]).toHaveProperty('role');
-            expect(user[0].role).toBe(Roles.CUSTOMER);
+            const users = await userRepository.find();
+            expect(users[0]).toHaveProperty('role');
+            expect(users[0].role).toBe(Roles.CUSTOMER);
+        });
+
+        it('should store the hashed password in the database', async () => {
+            //* Arrange:
+            const userData = {
+                firstName: 'Lovish',
+                lastName: 'Duggal',
+                email: 'lovishduggal11@gmail.com',
+                password: 'password',
+            };
+            //* Act:
+            await request(app).post('/auth/register').send(userData);
+            //* Assert:
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users[0].password).not.toBe(userData.password);
+            expect(users[0].password).toHaveLength(60);
+            expect(users[0].password).toMatch(/^\$2b\$\d+\$/);
+        });
+
+        it('should return 400 status code if email is already exits', async () => {
+            const userData = {
+                firstName: 'Lovish',
+                lastName: 'Duggal',
+                email: 'lovishduggal11@gmail.com',
+                password: 'password',
+            };
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({ ...userData, role: Roles.CUSTOMER });
+            //* Act:
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+            const users = await userRepository.find();
+            //* Assert:
+            expect(response.statusCode).toBe(400);
+            expect(users).toHaveLength(1);
         });
     });
     describe('Fields are missing', () => {});
