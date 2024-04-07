8e80c78067b0afc752432cb37a750a73fd03f1a5
Lovish Duggal <lovishduggal11@gmail.com>
2024-04-07 10:48:37 +0530

    add firstName validation

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,6 +1,16 @@
 import { checkSchema } from 'express-validator';
 
 export default checkSchema({
+    firstName: {
+        errorMessage: 'First name is required',
+        notEmpty: true,
+        trim: true,
+    },
+    lastName: {
+        errorMessage: 'Last name is required',
+        notEmpty: true,
+        trim: true,
+    },
     email: {
         errorMessage: 'Email is required',
         notEmpty: true,
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -173,6 +173,25 @@ describe('POST /auth/register', () => {
             expect(response.statusCode).toBe(400);
             expect(users).toHaveLength(0);
         });
+
+        it('should return 400 status code if firstName field is missing', async () => {
+            //* Arrange:
+            const userData = {
+                firstName: '',
+                lastName: 'Duggal',
+                email: '',
+                password: 'password',
+            };
+            //* Act:
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+            //* Assert:
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(response.statusCode).toBe(400);
+            expect(users).toHaveLength(0);
+        });
     });
 
     describe('Fields are not in proper format', () => {
