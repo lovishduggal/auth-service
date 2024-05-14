0c694bf19026d4bbc391438c150354443d034d6c
Lovish Duggal <lovishduggal11@gmail.com>
2024-05-14 08:41:20 +0530

    add test for admin

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,10 @@
 import app from './app';
-import bcrypt from 'bcryptjs';
 import { Config } from './config';
 import { AppDataSource } from './config/data-source';
 import logger from './config/logger';
-import { User } from './entity/User';
-import { Roles } from './constants';
+import { ensureAdminUser } from './utils';
 
-async function ensureAdminUser() {
-    const adminEmail = Config.ADMIN_EMAIL;
-    const adminPassword = await bcrypt.hash(Config.ADMIN_PASSWORD!, 10);
-
-    const userRepository = AppDataSource.getRepository(User);
-    const adminExists = await userRepository.findOneBy({
-        email: adminEmail,
-    });
-    if (!adminExists) {
-        const adminUser = {
-            firstName: Config.FIRST_NAME,
-            lastName: Config.LAST_NAME,
-            email: adminEmail,
-            password: adminPassword,
-            role: Roles.ADMIN,
-        };
-        await userRepository.save(adminUser);
-
-        logger.info('Admin user created successfully');
-    } else {
-        logger.info('Admin user already exists');
-    }
-}
-
-const startServer = async () => {
+export const startServer = async () => {
     const PORT = Config.PORT;
 
     try {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,29 @@
-export const calculateDiscount = (price: number, discount: number) => {
-    return price * (1 - discount / 100);
-};
+import bcrypt from 'bcryptjs';
+import { Config } from './config';
+import { AppDataSource } from './config/data-source';
+import { User } from './entity/User';
+import logger from './config/logger';
+import { Roles } from './constants';
+
+export async function ensureAdminUser() {
+    const adminEmail = Config.ADMIN_EMAIL;
+    const adminPassword = await bcrypt.hash(Config.ADMIN_PASSWORD!, 10);
+
+    const userRepository = AppDataSource.getRepository(User);
+    const adminExists = await userRepository.findOneBy({
+        email: adminEmail,
+    });
+    if (!adminExists) {
+        const adminUser = {
+            firstName: Config.FIRST_NAME,
+            lastName: Config.LAST_NAME,
+            email: adminEmail,
+            password: adminPassword,
+            role: Roles.ADMIN,
+        };
+        await userRepository.save(adminUser);
+        logger.info('Admin user created successfully');
+    } else {
+        logger.info('Admin user already exists');
+    }
+}
